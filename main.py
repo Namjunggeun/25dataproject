82036a7a0e06120d3aa719d42fadfd639416743c
Namjunggeun <711njg@hanmail.net>
2025-06-18 16:53:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import koreanize_matplotlib  # 한글 깨짐 방지
+#import koreanize_matplotlib  # 한글 깨짐 방지
 
 st.set_page_config(page_title="🗺️ 지역별 인구 구조 대시보드", layout="wide")
 
